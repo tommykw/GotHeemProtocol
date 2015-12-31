ec07c5d037fe6a13564b19f1a4ef42ba77e380ed
kenji tomita <destinys.lotus.1108mn@gmail.com>
2015-12-31 13:49:50 +0900

    settings IP

--- tcp_stack/TcpStack.scala
+++ tcp_stack/TcpStack.scala
@@ -1,5 +1,11 @@
 object TcpStack {
   def main(args: Array[String]) {
     println("not implements yet")
+
+    val srcIp = "127.0.0.1"
+    val dstIp = "127.0.0.1:8080"
+    // SYN
+    // ACK
   }
+
 }
