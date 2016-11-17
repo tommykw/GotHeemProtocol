5491fedfb5b70f09dfb7dd62f1a13e65080fc577
kenji tomita <destinys.lotus.1108mn@gmail.com>
2016-11-18 08:47:33 +0900

    add init && process

--- compiler/src/main/java/com/github/tommykw/compiler/Processor.java
+++ compiler/src/main/java/com/github/tommykw/compiler/Processor.java
@@ -1,5 +1,10 @@
 package com.github.tommykw.compiler;
 
+import com.github.tommykw.library.Generatable;
+
+import java.io.IOException;
+import java.util.logging.Filter;
+
 import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.Filter;
 import javax.annotation.processing.Messager;
@@ -17,4 +22,33 @@ import javax.lang.model.util.Elements;
 import javax.tools.Diagnostic;
 
 public class Processor extends AbstractProcessor {
+    private Filter filter;
+    private Messager messager;
+    private Elements elements;
+
+    @Override
+    public synchronized void init(ProcessingEnvironment processingEnv) {
+        super.init(processingEnv);
+        messager = processingEnv.getMessager();
+        filter = processingEnv.getFilter();
+        elements = processingEnv.getElementUtils();
+    }
+
+    @Override
+    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+        Class<Generatable> loggableClass = Generatable.class;
+        for (Element element : roundEnv.getElementsAnnotatedWith(loggableClass)) {
+            ElementKind kind = element.getKind();
+            if (kind == ElementKind.CLASS) {
+                try {
+                    generateLogger(element);
+                } catch (IOException e) {
+                    messager.printMessage(Dianostic.Kind.ERROR, "io error");
+                }
+            } else {
+                messager.printMessage(Diagnostic.Kind.ERROR, "type error");
+            }
+        }
+        return true;
+    }
 }
