915901633c3b03a621570dfa7d6ec7f6b01a1fcd
kenji tomita <destinys.lotus.1108mn@gmail.com>
2016-11-17 09:00:23 +0900

    Recompose dir

---
+++ compiler/src/main/java/com/github/tommykw/compiler/Processor.java
@@ -0,0 +1,20 @@
+package com.github.tommykw.compiler;
+
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Filter;
+import javax.annotation.processing.Messager;
+import javax.annotation.processing.ProcessingEnvironment;
+import javax.annotation.processing.Processor;
+import javax.annotation.processing.RoundEnvironment;
+import javax.annotation.processing.SupportedAnnotationTypes;
+import javax.annotation.processing.SupportedSourceVersion;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.ElementKind;
+import javax.lang.model.element.Modifier;
+import javax.lang.model.element.TypeElement;
+import javax.lang.model.util.Elements;
+import javax.tools.Diagnostic;
+
+public class Processor extends AbstractProcessor {
+}
